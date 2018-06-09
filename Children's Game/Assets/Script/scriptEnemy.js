e8dcf1aeaec79a1555418119141b0f044c9e70f1
Felipe Vilella <felipevilellaalves@hotmail.com>
2018-06-09 13:27:37 -0300

    update 09/06

--- Children's Game/Assets/Script/scriptEnemy.js
+++ Children's Game/Assets/Script/scriptEnemy.js
@@ -1,38 +1,51 @@
 #pragma strict
-// Enemy script
+//@RequireComponent(AudioSource)
 
-// Inspector variables
+
 var rndColors : Color[];
 var numberOfClicks : int = 2;
 var respawnTime : float = 1.0;
 var explosion : Transform;
 var enemyPoint : int = 1;
+var target:Transform;
+var sinal: int = 1;
+var speed: float = 0.01;
 
-// Private variables
-//if(numberOfClicks <= 0)
+//var audioSource: AudioSource;
+
+
+
+function Start() {
+	//audioSource = GetComponent.<AudioSource>();
+
+	if (transform.position.x > target.position.x)
+		sinal = -1;
+	else if (transform.position.x == target.position.x)
+		sinal = 0;
+}
+
 function Update () {
 	if(numberOfClicks <= 0)
 	{
-		if(explosion)
-		{
-			// Explosion
-			var myExplosion = Instantiate(explosion, transform.position, transform.rotation);
-		}
+		var deslocamentoX = Mathf.Abs(transform.position.x - target.position.x);
+		var deslocamentoY = Mathf.Abs(transform.position.y - target.position.y);
 		
+		Debug.Log("X:" + deslocamentoX  + " Y:"+ deslocamentoY);
 
+		transform.position.x = transform.position.x + (sinal * speed * deslocamentoX);
+		transform.position.y = transform.position.y - (speed * deslocamentoY);
 
+	}
+	
+	if (
+		(Mathf.Abs(transform.position.y - target.position.y) < 0.05) &&  
+		(Mathf.Abs(transform.position.x - target.position.x) < 0.05)
+	   ) {
+			numberOfClicks = 1;
+			//audioSource.Play();
+			Destroy(gameObject);
+	
 	}
 }
 
-function RespawnWaitTime()
-{
-	GetComponent.<Renderer>().enabled = false;
-	yield WaitForSeconds(respawnTime);
-	GetComponent.<Renderer>().enabled = true;
-}
 
-function RandomColor()
-{
-	var newColor = Random.Range(0, rndColors.length);
-	GetComponent.<Renderer>().material.color = rndColors[newColor];
-}
